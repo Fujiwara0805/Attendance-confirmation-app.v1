e7bae1310a6d6de12c9361962bfc5650524d2383
Fujiwara0805 <tiki4091@gmail.com>
2025-04-11 21:09:38 +0900

    ✨ 出席フォームの大学外からの出席登録メッセージを改善しました。

--- app/attendance/components/AttendanceForm.tsx
+++ app/attendance/components/AttendanceForm.tsx
@@ -102,7 +102,7 @@ export default function AttendanceForm() {
                   status: isOnCampus ? 'success' : 'outside',
                   message: isOnCampus 
                     ? 'キャンパス内から出席登録を行っています' 
-                    : '大学外から出席登録を行っています',
+                    : 'キャンパスの外から出席登録を行っています',
                   latitude: lat,
                   longitude: lng,
                   distance,
